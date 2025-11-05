49a2ce603f2c4a51199a3c1f1bc045548358a5a1
Genaro Barrera <genarobarrera1995@gmail.com>
2025-11-05 14:39:51 -0600

    Add Project P15_VariableDataTypes

---
+++ P15_IntegralTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿//An integral type is a simple value type that represents whole numbers with no fraction (such as -1, 0, 1, 2, 3).
+
+//Signed integral types 
+//A signed type uses its bytes to represent an equal number of positive and negative numbers
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+//Unsigned integral types
+//An unsigned type uses its bytes to represent only positive numbers
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+//A floating point is a simple value type that represents numbers to the right of the decimal place.
+
+
