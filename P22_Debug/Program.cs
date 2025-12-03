70e98ac4b6bf3241f1f329443c6a6bdb79926f31
Genaro Barrera <genarobarrera1995@gmail.com>
2025-12-03 17:20:33 -0600

    Add P23_DebugChallenge and add New Configurations for P23

--- P22_Debug/Program.cs
+++ P22_Debug/Program.cs
@@ -3,7 +3,7 @@ This code uses a names array and corresponding methods to display
 greeting messages
 */
 
-string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+/* string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
 
 string messageText = "";
 
@@ -33,8 +33,95 @@ static string AndrewMessage()
 {
     return "Hi, my name is Andrew. Good to meet you.";
 }
+ */
 
 //left off at Set Breakpoints section
 //ALRIGHT TIME TO DEBUG
 //This's one for github green 
 //do it all for the nookie
+
+int productCount = 2000; //number of products to process
+string[,] products = new string[productCount, 2]; //2D array to hold product data
+
+LoadProducts(products, productCount); //load product data into the products array
+
+for (int i = 0; i < productCount; i++)
+{
+    string result;
+    result = Process1(products, i); //breakpoint here, step into Process1
+
+    /* Take a moment to consider the advantage that conditional breakpoints offer. (51: products[i,1] == "new";)
+
+In this simulated data processing scenario, there is about a 1% chance that a product is new. If you're using a standard
+breakpoint to debug the issue, you'd need to walk through the analysis of about 100 products to find one of the new
+products that you're interested in.
+
+Conditional breakpoints can save you lots of time when you're debugging an application.
+ */
+
+    if (result != "obsolete")
+    {
+        result = Process2(products, i);
+    }
+}
+
+bool pauseCode = true;
+while (pauseCode == true) ;
+
+//Method to generate data for the simulated processes
+// The LoadProducts method generates 2000 random product IDs and assigns a value of existing, new, or obsolete to a
+// product description field. There is about a 1% chance that the products are marked new.
+static void LoadProducts(string[,] products, int productCount)
+{
+    Random rand = new Random();
+
+    for (int i = 0; i < productCount; i++)
+    {
+        int num1 = rand.Next(1, 10000) + 10000;
+        int num2 = rand.Next(1, 101);
+
+        string prodID = num1.ToString();
+
+        if (num2 < 91)
+        {
+            products[i, 1] = "existing";
+        }
+        else if (num2 == 91)
+        {
+            products[i, 1] = "new";
+            prodID = prodID + "-n";
+        }
+        else
+        {
+            products[i, 1] = "obsolete";
+            prodID = prodID + "-0";
+        }
+
+        products[i, 0] = prodID;
+    }
+}
+
+//Methods To simulate data processing
+// The Process1 and Process2 methods display progress messages and return a string.
+static string Process1(string[,] products, int item)
+{
+    Console.WriteLine($"Process1 message - working on {products[item, 1]} product");
+
+    return products[item, 1];
+}
+
+static string Process2(string[,] products, int item)
+{
+    Console.WriteLine($"Process2 message - working on product ID #: {products[item, 0]}");
+    if (products[item, 1] == "new")
+        Process3(products, item); //call Process3 if product is new
+
+    return "continue";
+}
+
+//Process3 method to handle new products. Displays a message indicating that new product information is being processed.
+static void Process3(string[,] products, int item)
+{
+    Console.WriteLine($"Process3 message - processing product information for 'new' product");
+}
+
